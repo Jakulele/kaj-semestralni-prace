54ff6e55a04652f8c902aa0a7c6a80cee2a4eb97
Jakub Ďuriš <durisja4@fel.cvut.cz>
2022-05-02 23:22:29 +0200

    Pain

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-function computeSubmit() {
+const submitButton = document.querySelector('#btnSubmit')
 
+submitButton.addEventListener('click', submit)
+
+function submit() {
+
+    const material = document.getElementById('material').value;
     const diameter = document.getElementById('diameter').value;
     const volume = document.getElementById('volume').value;
     // const glycol = document.getElementById('glycol').value;
+
+    findInternalPipeDia(material)
 
     const velocity = document.getElementById("velocityValue");
 
     const velocityResult = diameter * volume;
 
     velocity.textContent = velocityResult.toString();
+}
+
+function findInternalPipeDia(material) {
+
 }
