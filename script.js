9513aca9a39f3f6efe87d7aa715274efa037bf7b
Jakub Ďuriš <durisja4@fel.cvut.cz>
2022-05-25 13:36:54 +0200

    Add input checks, rework sizing guide, implement units select

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // calculations' support values
-const head = [10, 15, 20, 25, 32, 40, 50, 65, 80, 100, 150, 200, 250, 300, 350, 400, 450, 600]
+const head = [10, 15, 20, 25, 32, 40, 50, 65, 80, 100, 150, 200, 250, 300, 350, 400, 450, 600];
 const values = {
     "hg_bms": [11.2, 14.9, 20.4, 25.7, 34.3, 40.2, 51.3, 67, 79, 103.3, 154.3, 206.5, 258.8, 304.8, 336.6, 387.4, 438, 590],
     "mg_bms": [12.4, 16.2, 21.6, 27.3, 35.9, 41.9, 53, 68.7, 80.7, 105.1, 155.3, 208.3, 260.4, 309.6, 339.6, 390.4, 438, 590],
     "gms": [11.2, 14.9, 20.4, 25.7, 34.3, 40.2, 51.3, 67, 79, 103.3, 154.3, 206.5, 258.8, 304.8, 336.6, 387.4, 438, 590],
     "cu": [10.8, 13.6, 20.2, 26.2, 32.6, 39.6, 51.6, 64.3, 73.2, 105.1, 155.4, 0, 0, 0, 0, 0, 0, 0],
     "other": [10.8, 13.6, 20.2, 26.2, 32.6, 39.6, 51.6, 64.3, 73.2, 105.1, 155.4, 206.5, 258.8, 304, 336, 387, 438, 590]
-}
-const approx_size = [10, 15, 20, 25, 32, 40, 50, 65, 80, 100, 125, 150, 200, 250, 300, 350, 400, 450, 600]
-const FLOW_TEMPERATURE = 16
+};
+const approx_size = [10, 15, 20, 25, 32, 40, 50, 65, 80, 100, 125, 150, 200, 250, 300, 350, 400, 450, 600];
+const FLOW_TEMPERATURE = 16;
 
 const submitButton = document.querySelector('#btnSubmit')
 
@@ -17,12 +17,11 @@ submitButton.addEventListener('click', submit)
 function submit() {
     // user input values
     const material = document.getElementById('material').value;
-    const diameter = document.getElementById('diameter').value;
+    let diameter = document.getElementById('diameter').value;
+    diameter = convertDiameterUnits(diameter);
     let volume = document.getElementById('volume').value;
-    volume = volume / 3600
+    volume = convertVolumeUnits(volume);
     const glycol = document.getElementById('glycol').value;
-
-    checkInput(diameter, volume, glycol)
 
     // selecting result fields
     const velocity = document.getElementById("velocityValue");
@@ -30,6 +29,16 @@ function submit() {
     const eqL = document.getElementById("eqValue");
     const approxSize = document.getElementById("approxSize");
     const svg = document.querySelector('svg');
+
+    const valid = checkInput(diameter, volume, glycol)
+    if (!valid) {
+        velocity.textContent = "0";
+        prDrop.textContent = "0";
+        eqL.textContent = "0";
+        approxSize.textContent = "0";
+        svg.innerHTML = "";
+        return
+    }
 
     // result calculations
     const internalDiameter = findInternalPipeDia(material, diameter);
@@ -43,26 +52,54 @@ function submit() {
     let eqLValue = calculateEqL(relativeDensity, velocityResult, prDropValue);
 
     // update values to result fields
-    drawCircle(approxSizeValue, svg);
     velocity.textContent = velocityResult.toFixed(2).toString();
     prDrop.textContent = prDropValue.toFixed(0).toString();
     eqL.textContent = eqLValue.toFixed(1).toString();
     approxSize.textContent = approxSizeValue.toString();
+    const approxSizeMid = (approxSizeValue.length / 2) | 0;
+    drawCircle(approxSizeValue[approxSizeMid], svg);
+}
+
+function convertDiameterUnits(diameter) {
+    const diameterUnits = document.getElementById('diaUnits');
+    if (diameterUnits.value === "cm") {
+        return diameter * 10;
+    }
+    if (diameterUnits.value === "dm") {
+        return diameter * 100;
+    }
+    return diameter
+}
+
+function convertVolumeUnits(volume) {
+    const volumeUnits = document.getElementById('volumeUnits');
+    if (volumeUnits.value === "kgh") {
+        return volume / 3600;
+    }
+    return volume
 }
 
 function checkInput(diameter, volume, glycol) {
-    let textDiameter;
-    let textVolume;
-    let textGlycol;
-    if (isNaN(diameter) || diameter < 0) {
-        textDiameter = "Hodnota musí být větší než 0";
+    let valid = true;
+    const diameterError = document.getElementById("diameterError");
+    const volumeError = document.getElementById("volumeError");
+    const percentageError = document.getElementById("percentageError");
+    diameterError.textContent = "";
+    volumeError.textContent = "";
+    percentageError.textContent = "";
+    if (!(diameter > 0)) {
+        valid = false;
+        diameterError.textContent = "Hodnota musí být větší než 0";
     }
-    if (isNaN(volume) || diameter < 0) {
-        textVolume = "Hodnota musí být větší než 0";
+    if (!(volume > 0)) {
+        valid = false;
+        volumeError.textContent = "Hodnota musí být větší než 0";
     }
-    if (isNaN(glycol) || diameter < 0) {
-        textGlycol = "Neplatná hodnota";
+    if (!(glycol >= 0 && glycol <= 100)) {
+        valid = false;
+        percentageError.textContent = "Neplatná hodnota";
     }
+    return valid;
 }
 
 function findInternalPipeDia(material, diameter) {
@@ -159,48 +196,59 @@ function calculateEqL(relativeDensity, velocity, prDrop) {
 
 function approximateSize(volume) {
     let n = 0;
+    let results = [];
     for (let size of approx_size) {
         n = (volume / 1000) / (3.14159 * Math.pow(size / 2000, 2))
         if (size === 15) {
             if (n > 0.65 && n < 1.5) {
-                return size;
+                results.push(size);
             }
             continue
         }
         if (size <= 50) {
             if (n > 0.75 && n < 1.5) {
-                return size;
+                results.push(size);
             }
             continue
         }
         if (size <= 65) {
             if (n > 0.85 && n < 3) {
-                return size;
+                results.push(size);
             }
             continue
         }
         if (size > 65) {
             if (n > 1.25 && n < 3) {
-                return size;
+                results.push(size);
             }
         }
     }
+    if (results.length === 0) {
+        results.push("Undefined")
+    }
+    return results;
 }
 
 function drawCircle(r, svg) {
     svg.innerHTML = "";
+    if (isNaN(r)) {
+        return
+    }
+    if (r > 50) {
+        r = 50
+    }
     const outer = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    outer.setAttributeNS(null, "cx", "100");
-    outer.setAttributeNS(null, "cy", "100");
+    outer.setAttributeNS(null, "cx", "50");
+    outer.setAttributeNS(null, "cy", "50");
     outer.setAttributeNS(null, "r", r);
-    outer.setAttributeNS(null, "fill", "darkgrey");
+    outer.setAttributeNS(null, "fill", "grey");
     svg.appendChild(outer);
     const rs = r - r / 50;
     const inner = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    inner.setAttributeNS(null, "cx", "100");
-    inner.setAttributeNS(null, "cy", "100");
+    inner.setAttributeNS(null, "cx", "50");
+    inner.setAttributeNS(null, "cy", "50");
     inner.setAttributeNS(null, "r", rs.toString());
-    inner.setAttributeNS(null, "fill", "darkorange");
+    inner.setAttributeNS(null, "fill", "darkgrey");
     svg.appendChild(inner);
 }
 
